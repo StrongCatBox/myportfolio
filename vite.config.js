268b040045f83fcadb691403077cd9deeaee3aa6
Jesa <salam-jei@hotmail.fr>
2025-10-27 00:57:24 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/myportfolio/', // IMPORTANT pour GitHub Pages (repo StrongCatBox/myportfolio)
 })
